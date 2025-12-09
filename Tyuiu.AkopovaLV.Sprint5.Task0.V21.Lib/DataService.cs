e5629bd733c940476ad95bf9d9437ea5bf5bae4a
AkopovaLuda <ludmilaakopova27@gmail.com>
2025-12-09 08:11:30 -1200

    Task0

--- Tyuiu.AkopovaLV.Sprint5.Task0.V21.Lib/DataService.cs
+++ Tyuiu.AkopovaLV.Sprint5.Task0.V21.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.AkopovaLV.Sprint5.Task0.V21.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = @"C:\Users\Admin\source\repos\Tyuiu.AkopovaLV.Sprint5\Tyuiu.AkopovaLV.Sprint5.Task0.V21\bin\Debug\net8.0\OutPutFileTask0.txt";
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
             double y = Math.Round((x * x + 1) / Math.Sqrt(4 * x * x - 3), 3);
             File.WriteAllText(path, Convert.ToString(y));
             return path;
