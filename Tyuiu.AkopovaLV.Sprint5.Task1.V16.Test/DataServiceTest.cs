c0a8f66c78ceeeca6026f6b82df31990cce7d8f7
AkopovaLuda <ludmilaakopova27@gmail.com>
2025-12-09 11:16:26 -1200

    Task1

---
+++ Tyuiu.AkopovaLV.Sprint5.Task1.V16.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿namespace Tyuiu.AkopovaLV.Sprint5.Task1.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\Admin\source\repos\Tyuiu.AkopovaLV.Sprint5\Tyuiu.AkopovaLV.Sprint5.Task1.V16\bin\Debug\net8.0\OutPutFileTask1.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.AkopovaLV.Sprint5.Task1.V16/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.AkopovaLV.Sprint5.Task1.V16.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #5 | Выполнила: Акопова Л. В. | АСОиУб-25-1";
+        Console.WriteLine("****************************************************************************************************");
+        Console.WriteLine("* Спринт #5                                                                                        *");
+        Console.WriteLine("* Тема: Класс File. Запись набора данных в текстовый файл                                          *");
+        Console.WriteLine("* Задание #1                                                                                       *");
+        Console.WriteLine("* Вариант #16                                                                                      *");
+        Console.WriteLine("* Выполнила: Акопова Людмила Владимировна | АСОиУб-25-1                                           *");
+        Console.WriteLine("****************************************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                                         *");
+        Console.WriteLine("* Дана функция на заданном диапазоне. Произвести проверку деления на 0. Результат сохранить в      *");
+        Console.WriteLine("* текстовый файл и вывести на консоль таблицу. Значения округлить до 2 знаков после запятой        *");
+        Console.WriteLine("*                                                                                                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int start = -5, end = 5;
+        Console.WriteLine("Начало диапазона:" + start);
+        Console.WriteLine("Конец диапазона: " + end);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        string res = ds.SaveToFileTextData(start, end);
+
+        Console.WriteLine("Файл: " + res);
+        Console.WriteLine("Создан!");
+        Console.ReadKey();
+    }
+}
