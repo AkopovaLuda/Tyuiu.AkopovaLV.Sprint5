bbe8d2bddade76d66d92736761500de8ef9f040d
AkopovaLuda <ludmilaakopova27@gmail.com>
2025-12-09 13:54:04 -1200

    Task3

---
+++ Tyuiu.AkopovaLV.Sprint5.Task3.V25.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿namespace Tyuiu.AkopovaLV.Sprint5.Task3.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\Admin\source\repos\Tyuiu.AkopovaLV.Sprint5\Tyuiu.AkopovaLV.Sprint5.Task3.V25\bin\Debug\net8.0\OutPutFileTask3.bin";
+            FileInfo fileInfo = new FileInfo(path);
+            bool exists = fileInfo.Exists;
+            Assert.IsTrue(exists);
+        }
+    }
+}
---
+++ Tyuiu.AkopovaLV.Sprint5.Task3.V25/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.AkopovaLV.Sprint5.Task3.V25.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #5 | Выполнил: Томилов Дмитрий Алексеевич | ПКТб-24-1";
+       
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Потоковый метод записи данных в бинарный файл                           *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #25                                                             *");
+        Console.WriteLine("* Выполнил: Томилов Дмитрий Алексеевич | ПКТб-24-1                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+
+        Console.WriteLine("*Дано выражение вычислить его значение при x = 3, результат сохранить в би*");
+        Console.WriteLine("*инарный файл OutPutFileTask3.bin и вывести на консоль. Округлить до трёх *");
+        Console.WriteLine("* знаков после запятой.                                                   *");
+
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int x = 3;
+        Console.WriteLine($"x = {x}");
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        DataService ds = new DataService();
+
+        var result = ds.SaveToFileTextData(x);
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+}
