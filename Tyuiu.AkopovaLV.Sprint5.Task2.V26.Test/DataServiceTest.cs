dd9642e6167c7d02d41b19ed0cc57b67fb554066
AkopovaLuda <ludmilaakopova27@gmail.com>
2025-12-09 13:13:29 -1200

    Task2

---
+++ Tyuiu.AkopovaLV.Sprint5.Task2.V26.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿namespace Tyuiu.AkopovaLV.Sprint5.Task2.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\Admin\source\repos\Tyuiu.AkopovaLV.Sprint5\Tyuiu.AkopovaLV.Sprint5.Task2.V26\bin\Debug\net8.0\OutPutFileTask2.csv";
+            FileInfo fileInfo = new FileInfo(path);
+            bool exists = fileInfo.Exists;
+            Assert.IsTrue(exists);
+        }
+    }
+}
