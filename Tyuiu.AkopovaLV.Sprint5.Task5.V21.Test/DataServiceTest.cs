850a2807b313900870b7a220efa8899af82951bf
AkopovaLuda <ludmilaakopova27@gmail.com>
2025-12-14 11:29:01 -1200

    Task5

---
+++ Tyuiu.AkopovaLV.Sprint5.Task5.V21.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.AkopovaLV.Sprint5.Task5.V21.Lib;
+namespace Tyuiu.AkopovaLV.Sprint5.Task5.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask5V21.txt";
+
+            FileInfo fl = new FileInfo(path);
+            bool fle = fl.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fle);
+        }
+    }
+}
